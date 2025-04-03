e140db6df54fa14489adfdacc080fa412fe3f0fc
Cheonyeoun <vigneshpandiyan08@gmail.com>
2025-04-03 15:23:05 +0530

    completed assignment

--- src/components/UpdateItem.jsx
+++ src/components/UpdateItem.jsx
@@ -1,10 +1,54 @@
+import { useState } from "react";
+
 const UpdateItem = ({ item }) => {
     // 1. Create a state for the form
     // 2. Create a function to handle the form submission
     // 3. Create a function to handle the form input changes
-
     // your code here
-    return null;
+
+
+    const [updatedValue, setUpdatedValue] = useState(item?.name || "");
+    const handleUpdate = async (e)=>{
+        e.preventDefault();
+        try{
+
+            const response = await fetch(``,{
+                method:"PUT",
+                headers:{
+                    "Content-Type":"application/json",
+                },
+                body:JSON.stringify({name:updatedValue}),
+            });
+
+
+            if (!response.ok) {
+                console.error("Failed to update item")
+            }
+            else{
+                console.log("Item updated successfully!")};
+        }
+        catch (error){
+            console.error("Error updating item:",error);
+                }
+    };
+
+    const handleInputChange = (e)=>{
+        setUpdatedValue(e.target.value);
+    }
+
+    return (
+        <form onSubmit={handleUpdate}>
+            <h3>Update Item</h3>
+            <input 
+                type="text"
+                value={updatedValue}
+                onChange={handleInputChange}
+                placeholder="Enter the New Name"
+
+            />
+            <button type="submit">Update</button>
+        </form>
+    );
 };
 
 export default UpdateItem;
